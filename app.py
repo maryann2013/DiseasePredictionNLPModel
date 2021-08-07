fb7bddce4b4a07483ed3bd82bf931bec54aeb97e
maryann2013 <58733603+maryann2013@users.noreply.github.com>
2021-08-07 17:38:52 +0400

    Update app.py
    updated

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask,request, url_for, redirect, render_template, jsonify
+from flask import Flask,request, url_for, redirect, render_template
 import warnings
 warnings.filterwarnings('ignore')
 import pandas as pd
